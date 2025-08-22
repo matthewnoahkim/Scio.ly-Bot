3ed26064c53277452061cbd8c9ef7c46c8c3b72f
matthewnoahkim <mattnoahkim@gmail.com>
2025-08-21 21:22:36 -0700

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const token = process.env.BOT_TOKEN || require('./config.json').token;
+const token = process.env.BOT_TOKEN || require('./.env').BOT_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
