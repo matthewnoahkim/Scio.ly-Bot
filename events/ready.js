9af4b6bd3aa6a8c9e08e7ae85a34b0239b617d46
matthewnoahkim <mattnoahkim@gmail.com>
2025-08-24 18:55:57 -0700

    fun status

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,22 @@
 const { Events, ActivityType } = require('discord.js');
+const statuses = [
+    { name: 'I'},
+    { name: 'love'},
+    { name: 'SciOly'}
+];
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} is ready! Serving ${client.guilds.cache.size} servers.`);
-		client.user.setActivity('Science Olympiad questions | scio.ly', {
+		client.user.setActivity('scio.ly', {
 			type: ActivityType.Playing,
 		});
+		let i = 0;
+    	setInterval(() => {
+        	client.user.setStatus(statuses[i].name);
+        	i = (i + 1) % statuses.length;
+    	}, 1000);
 	}
 };
