0019b9e080fbbc527a96ad6e448a021893e6e161
matthewnoahkim <mattnoahkim@gmail.com>
2025-10-03 17:30:34 -0700

    bug fixed

--- commands/utility/materialssciencenanomaterials.js
+++ commands/utility/materialssciencenanomaterials.js
@@ -4,7 +4,7 @@ const { createSciOlyCommand } = require('../../shared-command-utils');
 const COMMAND_NAME = 'materialssciencenanomaterials';
 const EVENT_NAME = 'Materials Science - Nanomaterials';
 const DIVISIONS = ['C'];
-const ALLOWED_SUBTOPICS = ['Types of Nanomaterials','Structure & Chemistry','Applications of Nanomaterials','Optical Properties','Electrical Properties','Thermal Properties','Mechanical Properties','Magnetic Properties','Surfaces & Interfaces','Diffusion & Crystal Defects','Diffraction & Scattering','Bottom-Up Synthesis','Top-Down Synthesis','Chemistry','Characterization','Theory','Spectroscop Techniques','Crystal Defects','Microscopy Techniques','UV-Vis Spectroscopy','Photoluminescence','Raman Spectroscopy','EDS','Mass Spectrometry','Physical Properties','Characterization Techniques','Modeling','Calculations','Experimental Methods','Size Effects on Mechanical Properties','Plasticity & Dislocations','Adhesion & Interfaces','Hardness & Wear','Advanced Topics','Fracture & Toughness','Fatigue & Cyclic Loading','Extreme Environments','Scattering','Absorption','Plasmons','Quantum Confinement','Color','Luminescence & Fluorescence','Reflection & Refraction','Transmission','Physics of Materials','Nanomaterials','Interfaces','Techniques'];
+const ALLOWED_SUBTOPICS = ['Spectroscopy Techniques','Applications of Nanomaterials','Structure & Chemistry','Surfaces & Interfaces','Optical Properties','Types of Nanomaterials','Mechanical Properties','Thermal Properties','Electrical Properties','Microscopy Techniques','Bottom-Up Synthesis','Magnetic Properties','Top-Down Synthesis'];
 const ALLOW_IMAGES = false;
 
 module.exports = createSciOlyCommand({
