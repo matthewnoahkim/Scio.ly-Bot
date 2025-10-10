5ee70212b02361847725d7c364e0691148f781dc
matthewnoahkim <mattnoahkim@gmail.com>
2025-10-10 11:10:33 -0700

    update

--- commands/utility/materialssciencenanomaterials.js
+++ commands/utility/materialssciencenanomaterials.js
@@ -2,7 +2,7 @@ const { createSciOlyCommand } = require('../../shared-command-utils');
 
 // Command configuration
 const COMMAND_NAME = 'materialssciencenanomaterials';
-const EVENT_NAME = 'Materials Science';
+const EVENT_NAME = 'Materials Science - Nanomaterials';
 const DIVISIONS = ['C'];
 const ALLOWED_SUBTOPICS = ['Spectroscopy Techniques','Applications of Nanomaterials','Structure & Chemistry','Surfaces & Interfaces','Optical Properties','Types of Nanomaterials','Mechanical Properties','Thermal Properties','Electrical Properties','Microscopy Techniques','Bottom-Up Synthesis','Magnetic Properties','Top-Down Synthesis'];
 const ALLOW_IMAGES = false;
